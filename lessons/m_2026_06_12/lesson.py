89861419799e4cb0ae904a9001ee856f7f9ceea2
VladislavSoren <vlad224vlgu@gmail.com>
2026-06-22 22:49:00 +0300

    Занятие: for and while

--- lessons/m_2026_06_12/lesson.py
+++ lessons/m_2026_06_12/lesson.py
@@ -1,3 +1,7 @@
-import sys
+PASSWORD = "123"
+password_from_user = ""
 
-sys.getsizeof()
+while PASSWORD != password_from_user:
+    password_from_user = input("введите корректный пароль: ")
+
+print("введён корректный пароль")
